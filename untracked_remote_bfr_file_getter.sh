1051f2018ecfa226f5f4b1085cbb4025d8385708
Rob Nugen <rob@robnugen.com>
2021-04-12 18:42:31 +0900

    Able to get /quests/ files from server

---
+++ untracked_remote_bfr_file_getter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is designed to get files that exist on my server
+# but not included in this repo
+#
+# 12 April 2021
+#
+
+REMOTE_UNTRACKED_FILES="$1"
+
+# REMOTE_BFR_DIR should match ~/journal/untracked_remote_bfr_file_shower.sh
+REMOTE_BFR_DIR='~/barefoot_rob/content'         # must be in single quotes so ~ does not expand locally.
+LOCAL_BFR_DIR=~/barefoot_rob/content         # must not be in any quotes so ~ does expand locally.
+REMOTE_JUSTIN_CASE='~/untracked_files_copied_to_local_box'  # must be in single quotes so ~ does not expand locally.
+
+while read -r line; do
+    mkdir -p $(dirname $LOCAL_BFR_DIR/$line);
+    scp -F ~/.ssh/config_no_visual_keys bfr:$REMOTE_BFR_DIR/$line $LOCAL_BFR_DIR/$line
+    if [ $? -eq 0 ]; then
+        echo copy OK
+      	echo moving remote file to $REMOTE_JUSTIN_CASE
+      	# https://stackoverflow.com/a/9393147/194309 -n keeps ssh from breaking while loop
+      	ssh -n -F ~/.ssh/config_no_visual_keys bfr "mv $REMOTE_BFR_DIR/$line $REMOTE_JUSTIN_CASE"
+    else
+        echo copy FAIL
+        echo trying next file
+    fi
+done <<< "$REMOTE_UNTRACKED_FILES"
---
+++ untracked_remote_bfr_file_shower.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This is designed to show what files exist on my server under ~/barefoot_rob
+# but not included in the local repo
+#
+# 13 April 2021
+#
+# -F specifies a config file that does not show the ssh key visual fingerprint
+# bfr is my journal Micropub server endpoint defined in .ssh/config_no_visual_keys
+# git ls-files -o  shows untracked files on a line per line basis but does not handle spaces in filesnames  https://stackoverflow.com/a/8506155/194309
+
+# REMOTE_BFR_DIR should match ~/journal/untracked_remote_bfr_file_getter.sh
+REMOTE_BFR_DIR='~/barefoot_rob/content'    #REMOTE_BFR_DIR must be in single quotes so ~ does not expand locally.
+
+
+ssh -F ~/.ssh/config_no_visual_keys bfr "cd $REMOTE_BFR_DIR; git ls-files -o"
