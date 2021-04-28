4a4237e650ac283c1061aeff5ea6539a3e875708
Rob Nugen <rob@robnugen.com>
2021-04-29 07:11:43 +0900

    Bulk copy and move files on remote
    instead of angering the server with individual requests

--- untracked_remote_bfr_file_getter.sh
+++ untracked_remote_bfr_file_getter.sh
@@ -14,16 +14,21 @@ REMOTE_BFR_DIR='~/barefoot_rob/content'         # must be in single quotes so ~ 
 LOCAL_BFR_DIR=~/barefoot_rob/content         # must not be in any quotes so ~ does expand locally.
 REMOTE_JUSTIN_CASE='~/untracked_files_copied_to_local_box'  # must be in single quotes so ~ does not expand locally.
 
+CONCAT_FILES_TO_COPY=""
+
 while read -r line; do
     mkdir -p $(dirname $LOCAL_BFR_DIR/$line);
-    scp -F ~/.ssh/config_no_visual_keys bfr:$REMOTE_BFR_DIR/$line $LOCAL_BFR_DIR/$line
-    if [ $? -eq 0 ]; then
-        echo copy OK
-      	echo moving remote file to $REMOTE_JUSTIN_CASE
-      	# https://stackoverflow.com/a/9393147/194309 -n keeps ssh from breaking while loop
-      	ssh -n -F ~/.ssh/config_no_visual_keys bfr "mv $REMOTE_BFR_DIR/$line $REMOTE_JUSTIN_CASE"
-    else
-        echo copy FAIL
-        echo trying next file
-    fi
+    CONCAT_FILES_TO_COPY="$CONCAT_FILES_TO_COPY $REMOTE_BFR_DIR/$line"
 done <<< "$REMOTE_UNTRACKED_FILES"
+    
+scp -F ~/.ssh/config_no_visual_keys bfr:"$CONCAT_FILES_TO_COPY" $LOCAL_BFR_DIR/
+
+if [ $? -eq 0 ]; then
+    echo copy OK
+    echo moving remote file to $REMOTE_JUSTIN_CASE
+    # https://stackoverflow.com/a/9393147/194309 -n keeps ssh from breaking while loop
+    ssh -n -F ~/.ssh/config_no_visual_keys bfr "mv $CONCAT_FILES_TO_COPY $REMOTE_JUSTIN_CASE"
+else
+    echo copy FAIL
+fi
+
