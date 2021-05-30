b832e5af1d82a65c8828b3a090065769b03b85fa
Rob Nugen <rob@robnugen.com>
2021-05-30 14:18:17 +0900

    Allow selecting type
    Because debugging is done for now
    
    This reverts commit dfc6bf1580d3b21a4ce0f47e529952f63fc1cf18.

--- generate_events.pl
+++ generate_events.pl
@@ -356,7 +356,7 @@ sub get_event_type() {
     }# $ii
 
     print "Enter the number of the type you want to select: ";
-    my $jj = 1; # read from user after done debugging <STDIN>;
+    my $jj = <STDIN>;
 
     $event_type = $event_types[$jj-1];
 
