f3e2b22ccdaed3136564522081260e4f1f41f52b
Rob Nugen <rob@robnugen.com>
2021-06-07 14:12:08 +0900

    let generate.pl create book chapters

--- generate_events.pl
+++ generate_events.pl
@@ -26,9 +26,11 @@ my $content_directory = "$home/barefoot_rob/content";
 my $blog_directory = "/blog";    #  appended to $content_directory when writing actual file.
 my $events_directory = "/events";    #  appended to $content_directory when writing actual file.
 my $niigata_walk_dir = "/quests/walk-to-niigata";    #  appended to $content_directory when writing actual file.
+my $slow_down_book_dir = "/quests/slow-down";    #  appended to $content_directory when writing actual file.
 
 my %event_template_files = (
     "blog_entry" => "$home/barefoot_rob/templates/blog_template.txt",
+    "book_chapter" => "$home/barefoot_rob/templates/book_chapter_template.txt",
     "weekly_alignment" => "$home/barefoot_rob/templates/event_weekly-alignment_template.txt",
     "walking_meditation" => "$home/barefoot_rob/templates/event_walking_meditation_template.txt",
     "quest_update" => "$home/barefoot_rob/templates/niigata_2021_walking_update.txt",
@@ -36,6 +38,7 @@ my %event_template_files = (
 
 my %event_output_directories = (
     "blog_entry" => $blog_directory,
+    "book_chapter" => $slow_down_book_dir,
     "weekly_alignment" => $events_directory,
     "walking_meditation" => $events_directory,
     "quest_update" => $niigata_walk_dir,
@@ -43,6 +46,7 @@ my %event_output_directories = (
 
 my %event_tag_hashes = (
     "blog_entry" => {"blog" => 1},
+    "book_chapter" => {"book" => 1},
     "weekly_alignment" => {"weekly" => 1, "alignment" => 1, "event" => 1},
     "walking_meditation" => {"walk" => 1, "meditation" => 1, "event" => 1},
     "quest_update" => {"walk" => 1, "update" => 1, "quest" => 1},
@@ -52,6 +56,7 @@ my %event_title_prefixes = (
     "blog_entry" => "",
     "weekly_alignment" => "Weekly Alignment - ",
     "walking_meditation" => "Walking Meditation ",
+    "book_chapter" => "",
     "quest_update" => "",
 );
 
