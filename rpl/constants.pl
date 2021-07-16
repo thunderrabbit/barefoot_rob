1c70057a147f0eb7f80597f49350cbd1c90fe6f1
Rob Nugen <rob@robnugen.com>
2021-07-16 19:35:06 +0900

    Share constants

--- rpl/constants.pl
+++ rpl/constants.pl
@@ -2,15 +2,15 @@
 
 use strict;
 
-my $home = $ENV{HOME};    # https://stackoverflow.com/a/18123035
+our $home = $ENV{HOME};    # https://stackoverflow.com/a/1451420/194309
 
-my $content_directory = "$home/barefoot_rob/content";
-my $blog_directory = "/blog";    #  appended to $content_directory when writing actual file.
-my $events_directory = "/events";    #  appended to $content_directory when writing actual file.
-my $niigata_walk_dir = "/quests/walk-to-niigata";    #  appended to $content_directory when writing actual file.
-my $slow_down_book_dir = "/quests/slow-down";    #  appended to $content_directory when writing actual file.
+our $content_directory = "$home/barefoot_rob/content";
+our $blog_directory = "/blog";    #  appended to $content_directory when writing actual file.
+our $events_directory = "/events";    #  appended to $content_directory when writing actual file.
+our $niigata_walk_dir = "/quests/walk-to-niigata";    #  appended to $content_directory when writing actual file.
+our $slow_down_book_dir = "/quests/slow-down";    #  appended to $content_directory when writing actual file.
 
-my %event_template_files = (
+our %event_template_files = (
     "blog_entry" => "$home/barefoot_rob/templates/blog_template.txt",
     "book_chapter" => "$home/barefoot_rob/templates/book_chapter_template.txt",
     "weekly_alignment" => "$home/barefoot_rob/templates/event_weekly-alignment_template.txt",
@@ -18,7 +18,7 @@ my %event_template_files = (
     "quest_update" => "$home/barefoot_rob/templates/niigata_2021_walking_update.txt",
 );
 
-my %event_tag_hashes = (
+our %event_tag_hashes = (
     "blog_entry" => {"blog" => 1},
     "book_chapter" => {"book" => 1},
     "weekly_alignment" => {"weekly" => 1, "alignment" => 1, "event" => 1},
@@ -26,7 +26,7 @@ my %event_tag_hashes = (
     "quest_update" => {"walk" => 1, "update" => 1, "quest" => 1},
 );
 
-my %event_title_prefixes = (
+our %event_title_prefixes = (
     "blog_entry" => "",
     "weekly_alignment" => "Weekly Alignment - ",
     "walking_meditation" => "Walking Meditation",
