302544958a9ebb5f752fc54ae07ed0c380676e88
Rob Nugen <rob@robnugen.com>
2021-07-17 12:04:27 +0900

    Fix human legible date

--- generate_events.pl
+++ generate_events.pl
@@ -87,7 +87,8 @@ my $mt3_episode_output = $event_template;
 
 # handle date separately
 $mt3_episode_output =~ s/^(date: .*)/date: $rpl::Functions::tz_date/im;
-$mt3_episode_output =~ s/human_date_here/${event_date_time->strftime("%A %d %B %Y")}/;
+my $human_date = $event_date_time->strftime("%A %d %B %Y");
+$mt3_episode_output =~ s/human_date_here/$human_date/;
 $mt3_episode_output =~ s/%episode_image/$episode_image/;
 # do the rest algorithmically
 foreach my $key (keys %{ $new_entry }) {
