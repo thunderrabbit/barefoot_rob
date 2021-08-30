b95a1929ea4627af8793cb79415e9994353ec598
Rob Nugen <rob@robnugen.com>
2021-08-30 18:29:49 +0900

    Use new local base directory

--- rpl/Constants.pm
+++ rpl/Constants.pm
@@ -4,19 +4,19 @@ use strict;
 
 our $home = $ENV{HOME};    # https://stackoverflow.com/a/1451420/194309
 
-our $content_directory = "$home/barefoot_rob/content";
+our $content_directory = "$home/barefoot_rob_master/content";
 our $blog_directory = "/blog";    #  appended to $content_directory when writing actual file.
 our $events_directory = "/events";    #  appended to $content_directory when writing actual file.
 our $niigata_walk_dir = "/quests/walk-to-niigata";    #  appended to $content_directory when writing actual file.
 our $slow_down_book_dir = "/quests/slow-down";    #  appended to $content_directory when writing actual file.
 
 our %event_template_files = (
-    "blog_entry" => "$home/barefoot_rob/templates/blog_template.txt",
-    "book_chapter" => "$home/barefoot_rob/templates/book_chapter_template.txt",
-    "weekly_alignment" => "$home/barefoot_rob/templates/event_weekly-alignment_template.txt",
-    "walking_meditation" => "$home/barefoot_rob/templates/event_walking_meditation_template.txt",
-    "barefoot_walk" => "$home/barefoot_rob/templates/event_barefoot_walking.txt",
-    "quest_update" => "$home/barefoot_rob/templates/niigata_2021_walking_update.txt",
+    "blog_entry" => "$home/barefoot_rob_master/templates/blog_template.txt",
+    "book_chapter" => "$home/barefoot_rob_master/templates/book_chapter_template.txt",
+    "weekly_alignment" => "$home/barefoot_rob_master/templates/event_weekly-alignment_template.txt",
+    "walking_meditation" => "$home/barefoot_rob_master/templates/event_walking_meditation_template.txt",
+    "barefoot_walk" => "$home/barefoot_rob_master/templates/event_barefoot_walking.txt",
+    "quest_update" => "$home/barefoot_rob_master/templates/niigata_2021_walking_update.txt",
 );
 
 our %event_tag_hashes = (
