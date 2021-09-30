044dc9afb8e4d0f82f2e96d288e3440442ec3b3e
Rob Nugen <rob@robnugen.com>
2021-09-30 22:10:34 +0900

    Fill in Japanese dates

--- generate_events.pl
+++ generate_events.pl
@@ -121,7 +121,13 @@ foreach my $extension (keys %event_templates) {
   # handle date separately
   $mt3_episode_output =~ s/^(date: .*)/date: $rpl::Functions::tz_date/im;
   my $human_date = $event_date_time->strftime("%A %d %B %Y");
+  my $event_yyyy = $event_date_time->year;
+  my $event_m = $event_date_time->month;
+  my $event_d = $event_date_time->day;
   $mt3_episode_output =~ s/HUMANDATE/$human_date/;
+  $mt3_episode_output =~ s/EVENT_YYYY/$event_yyyy/g;
+  $mt3_episode_output =~ s/EVENT_M/$event_m/g;
+  $mt3_episode_output =~ s/EVENT_D/$event_d/g;
   $mt3_episode_output =~ s/episode_image/$episode_image/;
   # do the rest algorithmically
   foreach my $key (keys %{ $new_entry }) {
